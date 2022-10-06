bcf2e0c83d4ade4af363919e8c528abef16bb599
Arjun Aghera <arjun.199301134@muj.manipal.edu>
2022-10-06 16:21:40 +0530

    add: added functionality and components

--- src/components/ShareModal.tsx
+++ src/components/ShareModal.tsx
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { SearchModal } from "./SearchModal";
+import { SharedTo } from "./SharedTo";
 import { ShareTo } from "./ShareTo";
+import api from "../api";
+import { Profile } from "../types";
 
 type ShareModalProps = {
   setClicked: (active: boolean) => void;
 };
 
 export const ShareModal = ({ setClicked }: ShareModalProps) => {
+  const [profiles, setProfiles] = useState<Array<Profile>>([]);
+  const [isSearchModel, setIsSearchModel] = useState(false);
+  const handleSearch = () => {
+    setIsSearchModel(true);
+  };
+  const fecthData = async () => {
+    const response = await api("/profiles");
+    const data = await response.data;
+    setProfiles(data);
+  };
+  useEffect(() => {
+    fecthData();
+  }, []);
+  const noAccessProfiles = profiles.filter(
+    (profile) => profile.access === "No access"
+  );
+  const hasAccessProfiles = profiles.filter(
+    (profile) => profile.access !== "No access"
+  );
+  console.log(noAccessProfiles);
+  console.log(hasAccessProfiles);
   return (
-    <div className="w-1/3 max-h-96 shadow-xl rounded-lg border pt-1 border-brandLightGray">
-      <ShareTo />
+    <div className="w-1/3 max-h-96 shadow-xl rounded-lg border border-brandLightGray">
+      {!isSearchModel ? (
+        <>
+          <ShareTo />
+          <SharedTo handleSearch={handleSearch} profiles={hasAccessProfiles} />
+        </>
+      ) : (
+        <SearchModal profiles={noAccessProfiles} />
+      )}
     </div>
   );
 };
