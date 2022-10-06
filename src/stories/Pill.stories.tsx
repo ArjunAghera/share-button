1ea33c623d6125c780d651359a0def1cb279362e
Arjun Aghera <arjun.199301134@muj.manipal.edu>
2022-10-07 01:07:47 +0530

    add: story for Pill added

---
+++ src/stories/Pill.stories.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Pill } from "../components/Pill";
+
+export default {
+  title: "Example/Pill",
+  component: Pill,
+} as ComponentMeta<typeof Pill>;
+
+export const Primary: ComponentStory<typeof Pill> = () => (
+  <Pill
+    name="Arjun Aghera"
+    setSelected={(string) => {}}
+    setIsSelected={(boolean) => {}}
+  />
+);
