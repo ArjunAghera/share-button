a0030e1a099197fc67bce097caa37df47dd55b5d
Arjun Aghera <arjun.199301134@muj.manipal.edu>
2022-10-06 16:23:07 +0530

    add: added Pill component

---
+++ src/components/Pill.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import remove from "../public/x.png";
+
+interface PillProps {
+  name: string;
+  setIsSelected: (boolean: boolean) => void;
+  setSelected: (string: string) => void;
+}
+
+export const Pill = ({ name, setIsSelected, setSelected }: PillProps) => {
+  const handleRemove = () => {
+    setSelected("");
+    setIsSelected(false);
+  };
+  return (
+    <div className=" bg-brandMdGray flex flex-row m-2 p-2 rounded">
+      <p className=" text-brandDark text-sm">{name}</p>
+      <img
+        src={remove}
+        alt=""
+        onClick={handleRemove}
+        className=" h-2.5 w-2.5 ml-2 mt-1.5 cursor-pointer"
+      />
+    </div>
+  );
+};
