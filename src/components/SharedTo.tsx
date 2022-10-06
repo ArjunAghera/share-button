fa4f80ae4f4769e5632fe63a98dda20403997445
Arjun Aghera <arjun.199301134@muj.manipal.edu>
2022-10-06 16:17:16 +0530

    add: added componets and functionality

--- src/components/SharedTo.tsx
+++ src/components/SharedTo.tsx
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { Input } from "./Input";
+import { DetailedProfileWidget } from "./DetailedProfileWidget";
+import { Profile } from "../types";
 
-export const SharedTo = () => {
-  return <div>SharedTo</div>;
+interface SharedToProps {
+  handleSearch: (event: any) => void;
+  profiles: Array<Profile>;
+}
+
+export const SharedTo = ({ handleSearch, profiles }: SharedToProps) => {
+  const handleChange = () => {};
+  return (
+    <div>
+      <Input
+        placeholder="People, emails, groups"
+        onClick={handleSearch}
+        withDropdown={false}
+        onChange={handleChange}
+        value=""
+      />
+      <div>
+        {profiles.map((profile) => (
+          <>
+            <DetailedProfileWidget
+              title={profile.name}
+              subtitle={profile.detail}
+            />
+            <button></button>
+          </>
+        ))}
+      </div>
+    </div>
+  );
 };
