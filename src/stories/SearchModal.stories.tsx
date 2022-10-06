d10ab5faf2aa4fe8e28d6045e1400c0f7e3d9f9a
Arjun Aghera <arjun.199301134@muj.manipal.edu>
2022-10-07 01:08:04 +0530

    add: Story for SearchModal added

---
+++ src/stories/SearchModal.stories.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { SearchModal } from "../components/SearchModal";
+
+export default {
+  title: "Example/SearchModal",
+  component: SearchModal,
+  parameters: {
+    layout: "fullscreen",
+  },
+} as ComponentMeta<typeof SearchModal>;
+
+const profile = [
+  {
+    id: 1,
+    userName: "John Doe",
+    detail: "johndoe@sb.com",
+    access: "Full access",
+    type: "person",
+  },
+  {
+    id: 2,
+    userName: "Arjun Aghera",
+    detail: "arjunagehra@sb.com",
+    access: "Full access",
+    type: "person",
+  },
+];
+
+const Template: ComponentStory<typeof SearchModal> = (args) => (
+  <SearchModal {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  profiles: profile,
+};
