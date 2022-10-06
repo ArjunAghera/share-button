620eb9e1b9b65417fdef6e841acae02bd3269698
Arjun Aghera <arjun.199301134@muj.manipal.edu>
2022-10-07 01:09:53 +0530

    add: story for ShareTo added

---
+++ src/stories/ShareTo.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { ShareTo } from "../components/ShareTo";
+
+export default {
+  title: "Example/ShareTo",
+  component: ShareTo,
+} as ComponentMeta<typeof ShareTo>;
+
+export const Primary: ComponentStory<typeof ShareTo> = () => <ShareTo />;
