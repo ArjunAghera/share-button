55a6a6ba43c11b4a76eadbffcd757f6d6ad4083b
Arjun Aghera <arjun.199301134@muj.manipal.edu>
2022-10-07 01:10:36 +0530

    add: story for WidgetFooter added

---
+++ src/stories/WidgetFooter.stories.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { WidgetFooter } from "../components/WidgetFooter";
+
+export default {
+  title: "Example/WidgetFooter",
+  component: WidgetFooter,
+} as ComponentMeta<typeof WidgetFooter>;
+
+const Template: ComponentStory<typeof WidgetFooter> = (args) => (
+  <WidgetFooter {...args} />
+);
+
+export const WithCopyLink = Template.bind({});
+WithCopyLink.args = {
+  withCopyLink: true,
+};
+
+export const WithoutCopyLink = Template.bind({});
+WithoutCopyLink.args = {
+  withCopyLink: false,
+};
