0b4a482044d4cbd790e28ece4e28409912bbb64a
Arjun Aghera <arjun.199301134@muj.manipal.edu>
2022-10-06 16:22:40 +0530

    add: added functionality and customization options

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,17 +1,52 @@
-import React from "react";
+import { useState } from "react";
+import { AccessLevel } from "../data/accessLevel";
+import { Dropdown } from "./Dropdown";
 
 type InputProps = {
   placeholder: string;
+  onClick: (event: any) => void;
+  onChange: (event: any) => void;
+  value: string;
+  withDropdown?: boolean;
 };
 
-export const Input = ({ placeholder }: InputProps) => {
+export const Input = ({
+  placeholder,
+  onClick,
+  withDropdown,
+  onChange,
+  value,
+}: InputProps) => {
+  const bgColor = withDropdown
+    ? "bg-brandLightGray border-0"
+    : "bg-white border border-brandDarkGray ";
+  const options = [
+    AccessLevel.ALL,
+    AccessLevel.EDITOR,
+    AccessLevel.VIEWER,
+    AccessLevel.NONE,
+  ];
   return (
-    <div>
+    <div className="w-full my-4 flex justify-center">
       <input
-        type="text"
+        type="search"
         placeholder={placeholder}
-        className="border border-brandDarkGray rounded-md w-full h-10 ml-4 pl-4"
+        className={`rounded-l-md w-4/5 h-10 pl-4 ${bgColor}`}
+        value={value || ""}
+        onChange={onChange}
+        onClick={onClick}
       />
+      {withDropdown && (
+        <Dropdown
+          name="access"
+          options={options}
+          selected={AccessLevel.NONE}
+          bgColor="gray"
+        />
+      )}
+      <button className="px-4 py-1 text-lg bg-brandLightGray text-brandDark rounded-r-md">
+        Invite
+      </button>
     </div>
   );
 };
--- src/components/SearchModal.tsx
+++ src/components/SearchModal.tsx
@@ -1,32 +1,93 @@
 import React, { useState } from "react";
 import { Input } from "./Input";
-import { sharedTo } from "../data/sharedTo";
 import { SmallProfileWidget } from "./SmallProfileWidget";
+import { Profile } from "../types";
+import { Dropdown } from "./Dropdown";
+import { AccessLevel } from "../data/accessLevel";
+import { Pill } from "./Pill";
 
-export const SearchModal = () => {
+interface SearchModalProps {
+  profiles: Array<Profile>;
+}
+
+export const SearchModal = ({ profiles }: SearchModalProps) => {
+  const [inputValue, setInputValue] = useState("");
+  const [selected, setSelected] = useState("");
+  const [isSelected, setIsSelected] = useState(false);
   const isGroup = true;
   const isPerson = true;
   const handleClick = () => {};
-  const shares = sharedTo.sharedTo;
+  const options = [
+    AccessLevel.ALL,
+    AccessLevel.EDITOR,
+    AccessLevel.VIEWER,
+    AccessLevel.NONE,
+  ];
+  const handleChange = (event: any) => {
+    setInputValue(event.target.value);
+  };
+  const handleSelectProfile = (name: string) => {
+    if (selected !== "") {
+      alert("You have already selected " + selected);
+    } else {
+      setSelected(name);
+      setIsSelected(true);
+    }
+  };
+  console.log(selected);
+  const searchedProfile = profiles.filter((profile) =>
+    profile.name.toLowerCase().includes(inputValue.toLowerCase())
+  );
+  const personProfiles = searchedProfile.filter(
+    (profile) => profile.type === "person"
+  );
+  const groupProfiles = searchedProfile.filter(
+    (profile) => profile.type === "group"
+  );
   return (
-    <div className=" text-brandDark pb-4">
-      <Input
-        placeholder="Search emails, names, or groups"
-        onClick={handleClick}
-        withDropdown={true}
-      />
+    <div className=" text-brandDark pb-4 overflow-y-scroll h-96 scroll font-brandDeafult">
+      {!isSelected ? (
+        <Input
+          placeholder="Search emails, names, or groups"
+          onClick={handleClick}
+          withDropdown={true}
+          onChange={handleChange}
+          value={inputValue}
+        />
+      ) : (
+        <div className="w-full my-4 flex justify-start items-center bg-brandLightGray">
+          <Pill
+            name={selected}
+            setIsSelected={setIsSelected}
+            setSelected={setSelected}
+          />
+          <div className=" ml-auto mr-6">
+            <Dropdown
+              name="access"
+              options={options}
+              selected={AccessLevel.NONE}
+              bgColor="gray"
+            />
+            <button className="px-2 py-0.5 ml-4 bg-white border border-brandDarkGray text-brandDark rounded-md">
+              Invite
+            </button>
+          </div>
+        </div>
+      )}
       {isPerson && (
         <div className=" ml-4 pt-4 pb-2">
           <p className=" pl-2 font-medium">Select a person</p>
-          {shares.map((share) => (
-            <SmallProfileWidget title={share.name} />
+          {personProfiles.map((profile) => (
+            <div onClick={() => handleSelectProfile(profile.name)}>
+              <SmallProfileWidget title={profile.name} />
+            </div>
           ))}
         </div>
       )}
       {isGroup && (
         <div className=" ml-4 pt-4 pb-2">
           <p className=" pl-2 font-medium">Select a group</p>
-          {shares.map((share) => (
+          {groupProfiles.map((share) => (
             <SmallProfileWidget title={share.name} />
           ))}
         </div>
