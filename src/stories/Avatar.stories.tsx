8d0cdf8333e23b225ba2a4f87a7f47b968aa57a5
Arjun Aghera <arjun.199301134@muj.manipal.edu>
2022-10-07 01:06:28 +0530

    add: story for Avatar added

---
+++ src/stories/Avatar.stories.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Avatar } from "../components/Avatar";
+
+export default {
+  title: "Example/Avatar",
+  component: Avatar,
+} as ComponentMeta<typeof Avatar>;
+
+const Template: ComponentStory<typeof Avatar> = (args) => <Avatar {...args} />;
+
+export const Large = Template.bind({});
+Large.args = {
+  character: "A",
+  size: "large",
+  rounded: "full",
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  character: "A",
+  size: "small",
+  rounded: "md",
+};
