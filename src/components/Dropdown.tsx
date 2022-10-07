c365c0049356c428bf90941291e2d9f7aa1ffddc
Arjun Aghera <arjun.199301134@muj.manipal.edu>
2022-10-07 19:04:36 +0530

    fix: default value selected and functional

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,23 +1,33 @@
 import React from "react";
 
-type DropdownProps = {
+interface DropdownProps {
   name: string;
   options: string[];
   selected: string;
   bgColor: "gray" | "white";
-};
+  onChange: (event: any) => void;
+}
 
 export const Dropdown = ({
   name,
   options,
   selected,
   bgColor,
+  onChange,
 }: DropdownProps) => {
   const filteredOptions = options.filter((option) => option !== selected);
   const backgroundColor = bgColor === "gray" ? "bg-brandLightGray" : "bg-white";
   return (
-    <select name={name} className={backgroundColor}>
-      <option value={selected} selected>
+    <select
+      name={name}
+      className={`${backgroundColor} outline-none w-28 `}
+      onChange={onChange}
+    >
+      <option
+        value={selected}
+        defaultValue={selected}
+        className=" outline-none"
+      >
         {selected}
       </option>
       {filteredOptions.map((option) => (
